3503c47cb124db9475d99f787c54ed645985e7e7
Thomas-Deloffre <72923708+Thomas-Deloffre@users.noreply.github.com>
2024-03-26 11:14:04 +0100

    Add files via upload

---
+++ Pokedex App/src/app/pokemon/add-pokemon/add-pokemon.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../pokemon';
+
+@Component({
+  selector: 'app-add-pokemon',
+  templateUrl: './add-pokemon.component.html',
+  styleUrl: './add-pokemon.component.scss'
+})
+export class AddPokemonComponent implements OnInit {
+
+  pokemon: Pokemon;
+
+  constructor() { }
+
+  ngOnInit(): void {
+      this.pokemon = new Pokemon();
+  }
+}
